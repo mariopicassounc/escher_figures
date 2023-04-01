cad06a3e12737b79819a849dead581291ac92e9a
Mateo Malpassi <mateo.malpassi@mi.unc.edu.ar>
2023-04-01 17:00:43 -0300

    Implementación mapDib, foldDib, 2 más (falta test)

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -76,7 +76,8 @@ cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
 cuarteto d1 d2 d3 d4 = (d1 /// d2) .-. (d3 /// d4)
 
 -- Una figura repetida con las cuatro rotaciones, superpuestas.
-encimar4 = undefined
+encimar4 :: Dibujo a -> Dibujo a
+encimar4 d = ((d ^^^ (rot45 d))^^^((r180 d)^^^(r270 d)))
 
 -- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
 -- No confundir con encimar4!
@@ -84,16 +85,38 @@ ciclar = undefined
 
 -- Estructura general para la semántica (a no asustarse). Ayuda: 
 -- pensar en foldr y las definiciones de Floatro a la lógica
-foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
+foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) -> 
        (Float -> Float -> b -> b -> b) -> 
        (Float -> Float -> b -> b -> b) -> 
        (b -> b -> b) ->
        Dibujo a -> b
-foldDib = undefined
+foldDib fig rot esp r45 ap jun enc dibujo = case dibujo of
+    Figura f -> fig f
+    Rotar d -> rot (foldDib fig rot esp r45 ap jun enc d)
+    Espejar d -> esp (foldDib fig rot esp r45 ap jun enc d)
+    Rot45 d -> r45 (foldDib fig rot esp r45 ap jun enc d)
+    Apilar x y d1 d2 -> ap x y (foldDib fig rot esp r45 ap jun enc d1) (foldDib fig rot esp r45 ap jun enc d2)
+    Juntar x y d1 d2 -> jun x y (foldDib fig rot esp r45 ap jun enc d1) (foldDib fig rot esp r45 ap jun enc d2)
+    Encimar d1 d2 -> enc (foldDib fig rot esp r45 ap jun enc d1) (foldDib fig rot esp r45 ap jun enc d2)
+
 
 -- Demostrar que `mapDib figura = id`
 mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
-mapDib = undefined
+mapDib f (Figura fig) = f fig
+mapDib f (Rotar d) = Rotar (mapDib f d)
+mapDib f (Espejar d) = Espejar (mapDib f d)
+mapDib f (Rot45 d) = Rot45 (mapDib f d)
+mapDib f (Apilar x y d1 d2) = Apilar x y (mapDib f d1) (mapDib f d2)
+mapDib f (Juntar x y d1 d2) = Juntar x y (mapDib f d1) (mapDib f d2)
+mapDib f (Encimar d1 d2) = Encimar (mapDib f d1) (mapDib f d2)
+
 
 -- Junta todas las figuras básicas de un dibujo.
-figuras = undefined
+figuras :: Dibujo a -> [Dibujo a]
+figuras (Figura f) = [Figura f]
+figuras (Rotar d) = figuras d
+figuras (Espejar d) = figuras d
+figuras (Rot45 d) = figuras d
+figuras (Apilar _ _ d1 d2) = figuras d1 ++ figuras d2
+figuras (Juntar _ _ d1 d2) = figuras d1 ++ figuras d2
+figuras (Encimar d1 d2) = figuras d1 ++ figuras d2
